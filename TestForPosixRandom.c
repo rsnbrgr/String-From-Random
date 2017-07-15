0684d66af3e6137807f048fcd540255fb47247f9
rsnbrgr <us@kumite.com>
2017-07-15 01:27:16 -0400

    TestForPosixRandom.c
    Tests a C compiler for the predictable POSIX-compliant non-ISO random() function.

---
+++ TestForPosixRandom.c
@@ -0,0 +1,21 @@
+/*
+This PUBLIC DOMAIN program tests a C compiler for a predictable POSIX-compliant
+non-ISO random() function.  See https://github.com/rsnbrgr/String-From-Random
+for full details.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define POSIXRANDOMFIRSTVALUE 1804289383
+
+int main(void)
+    {
+    if ((int)random() == POSIXRANDOMFIRSTVALUE)
+        printf("HURRAY!  Your C compiler appears to use the predictable POSIX-compliant non-ISO random() function!\n");
+     else
+        printf("UH-OH!  Your C compiler appears to use a different PRNG for its random() function.  Sorry.\n");
+
+    printf("See https://github.com/rsnbrgr/String-From-Random for full details.\n");
+    return 0;
+    }
